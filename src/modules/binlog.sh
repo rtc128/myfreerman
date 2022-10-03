f3b6ac00324d2398f1988268e9aa19b7160787b6
Rodrigo T. Cardoso <rodrigocardoso@tjrs.gov.br>
2022-10-03 15:34:02 -0300

    First rev with LIST TRANSACTIONS

--- src/modules/binlog.sh
+++ src/modules/binlog.sh
@@ -43,6 +43,22 @@ function binlog_do_list_events()
 	rm $OUTPUT
 }
 
+function binlog_do_list_transactions()
+{
+	_list_transactions_validate_params || return 1
+
+	OUTPUT=`mktemp /tmp/myfreerman.XXXXXX`
+	_list_local_transactions "$START_TIME" $OUTPUT
+	if [ $? -eq 1 ]; then
+		return 1
+	fi
+	if [ $? -ne 100 ]; then
+		_list_backup_transactions "$START_TIME" $OUTPUT || return 1
+	fi
+	sort $OUTPUT
+	rm $OUTPUT
+}
+
 function _list_backup_events()
 {
 	MIN_DATE="$1"
@@ -70,6 +86,44 @@ function _list_backup_events()
 		#if it is plain binlog, just call function
 		if file "$FPATH" | grep -wi "mysql" >/dev/null; then
 			_list_one_binlog_events "${FPATH}" "$MIN_DATE" "$OUTPUT"
+			if [ $? -eq 100 ]; then
+				break
+			fi
+			continue
+		fi
+		#unknown type
+		write_out "Not a binary log file [$FPATH]"
+		return 1
+	done
+}
+
+function _list_backup_transactions()
+{
+	MIN_DATE="$1"
+	OUTPUT="$2"
+	#if backup binlog dir doesn't exist, list empty
+	if ! [ -d "$BINLOG_BACKUP_DIR" ]; then
+		return
+	fi
+	LIST=`ls -r "$BINLOG_BACKUP_DIR"`
+	for F in $LIST; do
+		FPATH="$BINLOG_BACKUP_DIR/$F"
+		#if it is compressed, uncompress
+		if file "$FPATH" | grep -wi "compressed" >/dev/null; then
+			SEQ=`echo $F | cut -d . -f 2`
+			BINLOG=`mktemp /tmp/myfreerman.XXXXXX`
+			gunzip -c "$FPATH" >$BINLOG || { rm $BINLOG; return 1; }
+			_list_one_binlog_transactions "${BINLOG}" "$MIN_DATE" "$OUTPUT"
+			RC=$?
+			rm $BINLOG
+			if [ $RC -eq 100 ]; then
+				break
+			fi
+			continue
+		fi
+		#if it is plain binlog, just call function
+		if file "$FPATH" | grep -wi "mysql" >/dev/null; then
+			_list_one_binlog_transactions "${FPATH}" "$MIN_DATE" "$OUTPUT"
 			if [ $? -eq 100 ]; then
 				break
 			fi
@@ -174,6 +228,41 @@ function _list_local_events()
 	rm $BINLOG_LIST
 }
 
+function _list_local_transactions()
+{
+	START_TIME="$1"
+	OUTPUT="$2"
+
+	#check if server has binary logs
+	LOG_BIN=`get_server_config log_bin` || return 1
+	[ -n "$LOG_BIN" ] || return 1
+	if [ $LOG_BIN -eq 0 ]; then
+		write_out "Logging is disbled in server"
+		return 1
+	fi
+
+	#binlog directory
+	LOG_BIN_BASENAME=`mysql --socket="$SERVER_SOCKET" $TARGET_CRED_OPT_NOPWD -e "show variables like 'log_bin_basename'" | tail -n 1 | cut -f 2 -d$'\t'` || return 1
+	BINLOG_DIRECTORY="`dirname \"$LOG_BIN_BASENAME\"`"
+
+	#list binlogs
+	BINLOG_LIST=`mktemp /tmp/myfreerman.XXXXXX`
+	list_server_binlogs >$BINLOG_LIST || return 1
+
+	#for each binlog, from last to first
+	COUNT=`wc -l $BINLOG_LIST | cut -d \  -f 1`
+	for i in `seq 1 $COUNT`; do
+		NAME=`tail -n $i $BINLOG_LIST | head -n 1`
+		FULL_NAME="${BINLOG_DIRECTORY}/${NAME}"
+		_list_one_binlog_transactions "${FULL_NAME}" "$START_TIME" "$OUTPUT"
+		if [ $? -eq 100 ]; then
+			rm $BINLOG_LIST
+			return 100
+		fi
+	done
+	rm $BINLOG_LIST
+}
+
 function _list_one_binlog_events()
 {
 	FULL_PATH="$1"
@@ -188,7 +277,7 @@ function _list_one_binlog_events()
 	#sequence is the last 6 chars in binlog path
 	SEQ=${FULL_PATH:(-6)}
 
-	MARK_LIST=`mktemp /tmp/myfreerman.XXXXXX`
+	local MARK_LIST=`mktemp /tmp/myfreerman.XXXXXX`
 
 	if [ -n "$START_TIME" ]; then
 		#list only marks
@@ -230,6 +319,62 @@ function _list_one_binlog_events()
 	wait
 
 	rm $MARK_LIST
+}
+
+function _list_one_binlog_transactions()
+{
+	FULL_PATH="$1"
+	START_TIME="$2"
+	OUTPUT="$3"
+
+	#if binlog does not exist, just ignore it because probably binlogs were flushed
+	if ! [ -f "$FULL_PATH" ]; then
+		return
+	fi
+
+	#sequence is the last 6 chars in binlog path
+	SEQ=${FULL_PATH:(-6)}
+
+	local SQL=`mktemp /tmp/myfreerman.XXXXXX`
+
+	if [ -n "$START_TIME" ]; then
+		#list only marks
+		if [ -n "$END_TIME" ]; then
+			mysqlbinlog --defaults-file="$SERVER_CONFIG" -v --start-datetime="$START_TIME" --stop-datetime="$END_TIME" --result-file=$SQL "$FULL_PATH"
+		else
+			mysqlbinlog --defaults-file="$SERVER_CONFIG" -v --start-datetime="$START_TIME" --result-file=$SQL "$FULL_PATH"
+		fi
+	else
+		mysqlbinlog --defaults-file="$SERVER_CONFIG" -v --result-file=$SQL "$FULL_PATH"
+	fi
+
+	local LINE_LIST=`mktemp /tmp/myfreerman.XXXXXX`
+	grep -n '^BEGIN$' $SQL | cut -d : -f 1 >$LINE_LIST
+	TOTAL_LINES=`wc -l $LINE_LIST | cut -d \  -f 1`
+
+	#if mark list is empty, inform we are already before min date
+	if [ $TOTAL_LINES -eq 0 ]; then
+		rm $LINE_LIST
+		return 100
+	fi
+
+	TH_LINE_COUNT=$((TOTAL_LINES/PROCESS_THREADS))
+	#if not exact division, add one line for each thread
+	if [ $(expr $TOTAL_LINES % $PROCESS_THREADS) != "0" ]; then
+		((TH_LINE_COUNT++))
+	fi
+
+	START_LINE=1
+	for I in `seq 1 $PROCESS_THREADS`; do
+		_list_one_binlog_transactions_th $LINE_LIST $SQL $START_LINE $TH_LINE_COUNT "$OUTPUT" &
+		START_LINE=$((START_LINE+TH_LINE_COUNT))
+	done
+	wait
+
+	if [ "$DEBUG" != "1" ]; then
+		rm $LINE_LIST
+		rm $SQL
+	fi
 }
 
 function _list_one_binlog_events_th()
@@ -293,6 +438,69 @@ function _list_one_binlog_events_th()
 		fi
 	done <$MARK_LIST
 	rm $MARK_LIST
+}
+
+function _list_one_binlog_transactions_th()
+{
+	local LINE_LIST=$1
+	local SQL=$2
+	local TH_START_LINE=$3
+	local TH_LINE_COUNT=$4
+	local OUTPUT=$5
+
+	local INS_COUNT
+	local UPD_COUNT
+	local DEL_COUNT
+	local TOTAL
+	
+	local LINES_TOTAL_LINES=`wc -l $LINE_LIST | awk '{ print $1; }'`
+	local SQL_TOTAL_LINES=`wc -l $SQL | awk '{ print $1; }'`
+	local LINES_START_LINE=$TH_START_LINE
+
+	for I in `seq 1 $TH_LINE_COUNT`; do
+		#if our first line is greater than total number of lines of the file, just ignore it
+		if [ $LINES_START_LINE -gt $LINES_TOTAL_LINES ]; then
+			return
+		fi
+
+		SQL_CUR_LINE=`head -n $LINES_START_LINE $LINE_LIST | tail -n 1`
+		TCOUNT=`expr $SQL_TOTAL_LINES - $SQL_CUR_LINE + 1`
+		COMMIT_LINE=`tail -n $TCOUNT $SQL | grep -nwi -m 1 ^commit | cut -d : -f 1`
+
+		#timestamp is in the last line with "at" before the line with "BEGIN"
+		TIMESTAMP=`head -n $SQL_CUR_LINE $SQL | tail -n 6 | grep -A 1 -w at | tail -n 1 | cut -d \  -f 2`
+		#TIMESTAMP=`head -n $SQL_CUR_LINE $SQL | tail -n 4 | head -n 1 | cut -d \  -f 2`
+
+		TXN=`mktemp /tmp/myfreerman.XXXXXX`
+		tail -n $TCOUNT $SQL | head -n $COMMIT_LINE >$TXN
+
+		INS_COUNT=0
+		UPD_COUNT=0
+		DEL_COUNT=0
+
+		#for each line
+		while IFS= read -r LINE; do
+			#INSERT?
+			if [[ "$LINE" =~ "### INSERT ".* ]]; then
+				((INS_COUNT++))
+			fi
+			#UPDATE?
+			if [[ "$LINE" =~ "### UPDATE ".* ]]; then
+				((UPD_COUNT++))
+			fi
+			#DELETE?
+			if [[ "$LINE" =~ "### DELETE ".* ]]; then
+				((DEL_COUNT++))
+			fi
+		done <$TXN
+		if [ "$DEBUG" != "1" ]; then
+			rm $TXN
+		fi
+
+		TOTAL=$((INS_COUNT+UPD_COUNT+DEL_COUNT))
+		printf "%-12s %-7s %-7s %-7s %-7s\n" $TIMESTAMP $INS_COUNT $UPD_COUNT $DEL_COUNT $TOTAL >>"$OUTPUT"
+		((LINES_START_LINE++))
+	done
 }
 
 function _list_one_binlog_events_detailed()
@@ -383,6 +591,51 @@ function _list_one_binlog_events_detailed_th()
 		fi
 	done
 	rm $LOCAL_LIST
+}
+
+function _list_transactions_validate_params()
+{
+	#one of START TIME or MINUTES must be informed
+	if [ -z "$START_TIME" -a -z "$MINUTES" ]; then
+		write_out "At least 'start time' or 'minutes' must be informed"
+		return 1
+	fi
+
+	#if minutes informed, check it
+	#start time cannot be informed, end time cannot be informed
+	if [ -n "$MINUTES" ]; then
+		if [ $MINUTES -le 0 ]; then
+			write_out "Invalid number of minutes"
+			return 1
+		fi
+		if [ -n "$START_TIME" ]; then
+			write_out "'Start time' and 'minutes' cannot be informed together"
+			return 1
+		fi
+		if [ -n "$END_TIME" ]; then
+			write_out "'End time' and 'minutes' cannot be informed together"
+			return 1
+		fi
+		START_TIME=`date --date="-$MINUTES minutes" +"%F %T"`
+	fi
+
+	#if end timestamp is informed, start timestamp must also be
+	if [ -n "$END_TIME" ]; then
+		if [ -z "$START_TIME" ]; then
+			write_out "When 'end time' is informed, 'start time' must also be informed"
+			return 1
+		fi
+		END_TIME=`expand_timestamp $END_TIME`
+		END_TIME="${END_TIME/_/ }"
+		[ -n "$END_TIME" ] || return 1
+	fi
+
+	#if start timestamp is informed (and not minutes), expand it
+	if [ -n "$START_TIME" -a -z "$MINUTES" ]; then
+		START_TIME=`expand_timestamp $START_TIME`
+		[ -n "$START_TIME" ] || return 1
+		START_TIME="${START_TIME/_/ }"
+	fi
 }
 
 function _mount_end_time_option()
