819fb74b9e3ea47d42009e3e043defaf13557108
Rodrigo T. Cardoso <rodrigocardoso@tjrs.gov.br>
2022-10-06 12:30:32 -0300

    Starting to take locking system to a module

---
+++ src/modules/lock.sh
@@ -0,0 +1,22 @@
+function lock_binlog_lock()
+{
+	LOCK_FILE="$WORKING_DIR/binlog.lock"
+	MAX_WAIT_TIME_MIN=10
+	SLEEP_TIME=15
+
+	MAX_WAIT_TIME_SEC=`expr $MAX_WAIT_TIME_MIN \* 60`
+	TOT_WAIT_TIME=0
+
+	if [ -n "$LOCK_FILE" ]; then
+		while [ -f "$LOCK_FILE" ]; do
+			if [ $TOT_WAIT_TIME -ge $MAX_WAIT_TIME_SEC ]; then
+				write_out "Timeout reached waiting for another instance"
+				return 1
+			fi
+			write_out "Waiting for another instance to access binary logs - lock file: $LOCK_FILE"
+			sleep $SLEEP_TIME
+			TOT_WAIT_TIME=`expr $TOT_WAIT_TIME + $SLEEP_TIME`
+		done
+		touch "$LOCK_FILE" || return 1
+	fi
+}
